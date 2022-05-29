c3e17e8dfc6045518cd893524fc5cc7d56d6cde1
vadith gomez <legrone@MacBook-Pro.local>
2022-05-28 23:19:28 -0500

    learning

--- src/App.tsx
+++ src/App.tsx
@@ -9,37 +9,32 @@ interface AppState {
   newSubsNumber: number
 }
 
-const INITIAL_STATE = [
-  {
-    nick: 'dapelu',
-    subMonths: 3,
-    avatar: 'https://i.pravatar.cc/150?u=dapelu',
-    description: 'Dapelu hace de moderador a veces'
-  },
-  {
-    nick: 'sergio_serrano',
-    subMonths: 7,
-    avatar: 'https://i.pravatar.cc/150?u=sergio_serrano',
-  }
-]
-
 function App() {
   const [subs, setSubs] = useState<AppState["subs"]>([]);
   const [newSubsNumber, setNewSubsNumber] = useState<AppState["newSubsNumber"]>(0);
   const divRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    setSubs(INITIAL_STATE);
+    /* const fetchSubs = (): promise<any> => {
+      return
+      fetch('https://randomuser.me/api/?results=5?nat=ch').then(res => res.json())
+    }
+      .then(subs => {
+        console.log(subs)
+        setSubs(subs.results)
+      }) */
   }, [])
 
-  const handleNewSub = ( newSub: Sub): void => {
-    setSubs(subs => [ ...subs, newSub ]);
+  const handleNewSub = (newSub: Sub): void => {
+    setSubs(subs => [...subs, newSub]);
+    setNewSubsNumber(n => n = 1)
   }
 
   return (
     <div className="App" ref={divRef}>
       <h1>midu subs</h1>
-      <List subs={subs}/>
+      <List subs={subs} />
+      New Subs: {newSubsNumber}
       <Form onNewSub={handleNewSub} />
     </div>
   );
